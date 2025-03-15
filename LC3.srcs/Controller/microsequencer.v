80c5128cfb9e986a2ba05738fea51be5e7ea41c9
Austin Dolan <adolan@ncsu.edu>
2025-03-15 17:09:20 -0400

    restructured controller

--- LC3.srcs/Controller/microsequencer.v
+++ LC3.srcs/Controller/microsequencer.v
@@ -24,12 +24,13 @@ module microsequencer(
 	input[15:11] instruction, //5 msb of state
 	input [5:0] J, //current state
 	input[2:0] COND, //conditions from microinstruction
+	input clk, reset_n,
 	output reg[5:0] control_store_index
 	);
 	
 	// TODO microsequencer ~ next state logic. index into control store ~ output assignment.
 	
-	reg[5:0] nextState;
+	reg[5:0] nextState, next_index;
 	
 	always@(*)begin //next state 
 		nextState[5] <=	J[5] | (~COND[0] & COND[1] & COND[2] & ACV);
@@ -39,8 +40,13 @@ module microsequencer(
 		nextState[1] <=	J[1] | (COND[0] & ~COND[1] & ~COND[2] & R);
 		nextState[0] <=	J[0] | (COND[0] & COND[1] & ~COND[2] & instruction[11]);
 		
-		control_store_index <= IRD ? {0,0,instruction[15:12] : nextState;
+		next_index <= IRD ? {0,0,instruction[15:12]} : nextState;
 	end
 	
-end
+	always@(posedge clk or negedge reset_n)begin
+		if(!reset_n) control_store_index <= 0;
+		else control_store_index <= next_index;
+	end
+	
+endmodule
 
